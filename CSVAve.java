0c99795fe63e77d59742a6f42d799be3ebacd584
Ahdieh Emadi <ahdieh_e@yahoo.com>
2019-01-31 12:27:17 -0500

    Created a method to represent the averahe temperature of the records with the humidity greater than a specific value

--- CSVAve.java
+++ CSVAve.java
@@ -27,4 +27,25 @@ public class CSVAve {
         System.out.println("Average temperature in file is " + average);
     }
     
-}
+    public double averageTemperatureWithHighHumidityInFile(CSVParser parser, int value){
+        double averageTempWithHighHumidity = 0;
+        double sum = 0;
+        int count = 0;
+        for (CSVRecord record : parser){
+            if (Double.parseDouble(record.get("Humidity")) >= value){
+                double temp = Double.parseDouble(record.get("TemperatureF"));
+                sum +=temp;
+                count += 1;
+            }
+        }
+        if (count != 0) averageTempWithHighHumidity = sum/count;
+        else System.out.println("No temperatures with that humidity");
+        return averageTempWithHighHumidity;
+    }
+    
+    public void testAverageTemperatureWithHighHumidityInFile(){
+       FileResource fr = new FileResource();
+       double averageTempWithHighHumidity = averageTemperatureWithHighHumidityInFile(fr.getCSVParser(), 80);
+       if (averageTempWithHighHumidity != 0) System.out.println("Average Temp when high Humidity is " + averageTempWithHighHumidity);
+    }
+} 
